ccb9dd5c932544ccc3214f4e3d4ae7cdbc02616b
John Claro <jkrclaro@gmail.com>
2021-04-13 00:24:13 +0100

    Update comments

--- fk/book.py
+++ fk/book.py
@@ -1,4 +1,5 @@
 import re
+from typing import Tuple
 
 from pyspark import SparkConf, SparkContext
 
@@ -7,20 +8,35 @@ sc = SparkContext(conf=conf)
 
 
 def normalize_words(text: str) -> list:
-    words = re.compile(r'\W+', re.UNICODE).split(text.lower())
-    return words
+    """Uses regular expression to search for proper words.
+
+    Args:
+        text: The whole book in txt format
+
+    Returns:
+        A list of lowercased words
+    """
+    return re.compile(r'\W+', re.UNICODE).split(text.lower())
 
 
-def init_occurences(word):
-    return (word, 1)
+def map_words(word: str) -> Tuple[str, int]:
+    """Maps the word with an increment value of 1.
+
+    Args:
+        word: A word in the book
+
+    Returns:
+        A tuple containing the word and an increment value of 1
+    """
+    return word, 1
 
 
-def increment_occurences(total: int, increment: int) -> int:
-    """Increments the total number of occurences by the 'increment' value.
+def count_words(total: int, increment: int) -> int:
+    """Increments the total number of occurrences by the 'increment' value.
 
     Args:
         total: Total number of times the word has occured
-        increment: Value returned by 'init_occurences' method
+        increment: Value returned by 'init_occurrences' method
 
     Returns:
         Number of times a word has occured
@@ -29,19 +45,22 @@ def increment_occurences(total: int, increment: int) -> int:
 
 
 def switch_position(record: tuple) -> tuple:
-    """Flips a record to enable sorting by word."""
-    word = record[0]
-    count = record[1]
-    return (count, word)
+    """Switches positions of word and count to sort by key which is count."""
+    return record[1], record[0]
 
 
-text = sc.textFile('fk/datasets/book.txt')
-words = text.flatMap(normalize_words)
-word_counts = words.map(init_occurences).reduceByKey(increment_occurences)
-word_counts_sorted = word_counts.map(switch_position).sortByKey()
-results = word_counts_sorted.collect()
-for result in results:
-    count = str(result[0])
-    word = result[1].encode('ascii', 'ignore')
-    if word:
-        print(f'{word}: \t\t{count}')
+def main():
+    text = sc.textFile('fk/datasets/book.txt')
+    words = text.flatMap(normalize_words)
+    word_counts = words.map(map_words).reduceByKey(count_words)
+    word_counts_sorted = word_counts.map(switch_position).sortByKey()
+    results = word_counts_sorted.collect()
+    for result in results:
+        count = str(result[0])
+        word = result[1].encode('ascii', 'ignore')
+        if word:
+            print(f'{word}: \t\t{count}')
+
+
+if __name__ == '__main__':
+    main()
