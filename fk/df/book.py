742d7eac71e24bc22308d622d5fd7d1989d1862f
John Claro <jkrclaro@gmail.com>
2021-04-13 18:15:05 +0100

    Add book

---
+++ fk/df/book.py
@@ -0,0 +1,28 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import explode, split, lower, desc
+
+
+def main():
+    spark = SparkSession.builder.appName('WordCount').getOrCreate()
+    df = spark.read.text('fk/datasets/book.txt')
+
+    # Split the line of text into list of words then use explode to create
+    # multiple rows for those list of words
+    words = df.\
+        select(
+            explode(split(df.value, '\\W+')).alias('word')
+        )
+    query = words. \
+        filter(words.word != '').\
+        select(
+            lower(words.word).alias('word')
+        ).\
+        groupBy('word').\
+        count().\
+        sort(desc('count'))
+    query.show()
+    spark.stop()
+
+
+if __name__ == '__main__':
+    main()
