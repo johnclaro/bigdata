a2d7df9e4109c0c62c7dba58d683ca142eca02d5
John Claro <jkrclaro@gmail.com>
2021-04-13 21:16:49 +0100

    Add movies

--- fk/rdd/movies.py
+++ fk/rdd/movies.py
@@ -10,7 +10,7 @@ def parse_data(x):
 def main():
     conf = SparkConf().setMaster('local').setAppName('RatingsHistogram')
     sc = SparkContext(conf=conf)
-    data = sc.textFile('fk/files/ratings/u.data')
+    data = sc.textFile('fk/files/movies/u.data')
     rdd = data.map(parse_data).countByValue()
     rdd = collections.OrderedDict(sorted(rdd.items()))
     for key, value in rdd.items():
