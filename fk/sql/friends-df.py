66e8d4929720c1e4a23ae1e6cadef9217b9a5d62
John Claro <jkrclaro@gmail.com>
2021-04-13 17:52:11 +0100

    Query friends using dataframes

---
+++ fk/sql/friends-df.py
@@ -0,0 +1,25 @@
+from typing import Tuple
+
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import avg, round
+
+
+def main():
+    spark = SparkSession.builder.appName('SparkSQL').getOrCreate()
+    data = spark.\
+        read.\
+        option('header', 'true').\
+        option('inferSchema', 'true').\
+        csv('fk/datasets/friends-header.csv')
+    query = data.\
+        groupBy('age').\
+        agg(
+            round(avg('friends'), 2).alias('friends_avg')
+        ).\
+        sort('age')
+    query.show()
+    spark.stop()
+
+
+if __name__ == '__main__':
+    main()
