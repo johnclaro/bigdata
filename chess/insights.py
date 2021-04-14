55f58ea60604ccc50a9043c82f60b09ee55d0d4a
John Claro <jkrclaro@gmail.com>
2021-04-14 05:00:34 +0100

    Query pgn using df

--- chess/insights.py
+++ chess/insights.py
@@ -1,16 +1,37 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import split, col, regexp_extract, regexp_replace
 
 
 def main():
     spark = SparkSession.builder.appName('Chess').getOrCreate()
-    chess = spark.\
-        read.\
-        option('header', 'true').\
-        option('inferSchema', 'true').\
-        csv('chess/files/jan2013.csv')
-
-    chess.printSchema()
-    chess.select('*').show()
+    r1 = spark.read.text('chess/files/july2016.pgn').\
+        withColumn(
+            'thing',
+            regexp_replace(
+                split(
+                    col('value'),
+                    ' '
+                )[0],
+                '\\[',
+                '',
+            )
+        ). \
+        withColumn(
+            'data',
+            regexp_extract(
+                col('value'),
+                '\\"(.*?)"',
+                1
+            )
+        ). \
+        select(
+            col('thing'),
+            col('data'),
+        )
+    r1.filter(
+        (col('thing') != '1.') &
+        (col('thing') != ''),
+    ).show()
     spark.stop()
 
 
