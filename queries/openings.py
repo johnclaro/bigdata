0ee03f38449606b6ac1428fbb521fe91761b8959
John Claro <jkrclaro@gmail.com>
2021-04-15 01:35:02 +0100

    Set individual elo ranges

--- queries/openings.py
+++ queries/openings.py
@@ -3,25 +3,10 @@ from pyspark.sql.functions import (
     col,
     regexp_extract,
     monotonically_increasing_id,
-    desc,
+    count,
     udf,
+    desc,
 )
-
-
-def set_rating_range(elo):
-    elo = int(elo)
-    rating_range = '< 1200' if elo < 1200 else \
-        '1200-1400' if 1200 <= elo <= 1400 else \
-        '1400-1600' if 1400 <= elo <= 1600 else \
-        '1600-1800' if 1600 <= elo <= 1800 else \
-        '1800-2000' if 1800 <= elo <= 2000 else \
-        '2000-2200' if 2000 <= elo <= 2200 else \
-        '2200-2300' if 2200 <= elo <= 2300 else \
-        '2300-2400' if 2300 <= elo <= 2400 else \
-        '2400-2500' if 2400 <= elo <= 2500 else \
-        '2500-2700' if 2500 <= elo <= 2700 else \
-        '2700+' if elo >= 2700 else ''
-    return rating_range
 
 
 def main():
@@ -58,18 +43,32 @@ def main():
             )
         views.append(view)
 
-    rating_range = udf(set_rating_range)
+    elo_below_1200 = udf(lambda elo: '1' if int(elo) < 1200 else '0')
+    elo_1200_1400 = udf(lambda elo: '1' if 1200 <= int(elo) < 1400 else '0')
+    elo_1400_1600 = udf(lambda elo: '1' if 1400 <= int(elo) < 1600 else '0')
+
     combined = views[0].join(views[1], ['game_id']).join(views[2], ['game_id'])
     combined = combined.\
         withColumn(
-            'rating_range',
-            rating_range(combined.white_elo)
+            '<1200',
+            elo_below_1200(combined.white_elo)
+        ).\
+        withColumn(
+            '1200-1400',
+            elo_1200_1400(combined.white_elo)
+        ).\
+        withColumn(
+            '1400-1600',
+            elo_1400_1600(combined.white_elo)
         ).\
         select(
             col('opening'),
-            col('rating_range')
+            col('<1200'),
+            col('1200-1400'),
+            col('1400-1600'),
         )
-    combined.show(10, truncate=False)
+
+    combined.show(truncate=False)
 
     spark.stop()
 
