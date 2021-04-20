bdcb704abf20abe354fe218b0f321c601022cbb4
John Claro <jkrclaro@gmail.com>
2021-04-20 23:56:44 +0100

    Change to plies

--- queries/plies.py
+++ queries/plies.py
@@ -1,4 +1,5 @@
 from timeit import default_timer as timer
+from datetime import timedelta
 
 from pyspark.sql import functions as f
 from pyspark.sql import SparkSession
@@ -10,15 +11,20 @@ from helpers import transform, show_or_save
 def extract(df: DataFrame):
     start = timer()
     df = df.\
-        filter(
-            (f.col('BlackElo').isNotNull()) &
-            (f.col('WhiteElo').isNotNull())
+        withColumn(
+            'Plies',
+            f.split(
+                'moves',
+                ' '
+            )[0]
         )
+
+    print(f'Extracting: {timedelta(seconds=timer() - start)}')
     return df
 
 
 def main():
-    query = 'survival'
+    query = 'plies'
     spark = SparkSession.builder.appName(query).getOrCreate()
     data = spark.read.text('datasets/test.pgn')
     df = transform(data)
