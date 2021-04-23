cee5094c6767a6199037517e2ee9c8b53349ba3d
John Claro <jkrclaro@gmail.com>
2021-04-23 20:17:41 +0100

    Add bishops and knights

---
+++ apps/knights.py
@@ -0,0 +1,9 @@
+from pyspark.sql.dataframe import DataFrame
+
+from helpers.heatmap import get_piece, group_by_piece
+
+
+def extract(df: DataFrame):
+    df = get_piece(df, 'N', 'Knights')
+    df = group_by_piece(df, 'Knights')
+    return df
