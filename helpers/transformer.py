da09151290cc882eabc658381936db7b959a54e4
John Claro <jkrclaro@gmail.com>
2021-04-23 21:07:46 +0100

    Use yield and change to default_schema

--- helpers/transformer.py
+++ helpers/transformer.py
@@ -1,11 +1,9 @@
-from collections import deque
-
 from pyspark.sql.dataframe import DataFrame
 
 from helpers.timer import timer
 
 
-schema = {
+default_schema = {
     'Event': '',
     'Site': '',
     'White': '',
@@ -38,14 +36,13 @@ def is_ply(ply):
 
 
 def reformat(partition):
-    new_df = deque()
-    columns = schema.copy()
+    schema = default_schema.copy()
     for row in partition:
         value = row.value
         if '"' in value:
             text = value.split('"')
             column = text[0][1:].replace(' ', '')
-            if column in schema.keys():
+            if column in default_schema.keys():
                 record = text[1]
                 if column in ('WhiteElo', 'BlackElo'):
                     try:
@@ -56,17 +53,15 @@ def reformat(partition):
                     record = record.split(' ')[1]
                 elif column == 'Site':
                     record = record.split('/')[-1]
-                columns[column] = record
+                schema[column] = record
         if '1. ' in value:
             notations = [
                 ply
                 for ply in value.split(' ')
                 if is_ply(ply)
             ]
-            columns['Notations'] = notations
-            new_df.append(list(columns.values()))
-            columns = schema.copy()
-    return iter(new_df)
+            schema['Notations'] = notations
+            yield list(schema.values())
 
 
 @timer
@@ -74,7 +69,7 @@ def transform(data: DataFrame):
     df = data.\
         rdd.\
         mapPartitions(reformat).\
-        toDF(list(schema.keys()))
+        toDF(list(default_schema.keys()))
 
     return df
 
