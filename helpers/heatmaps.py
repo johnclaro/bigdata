735e1fd0d6c0ff6907e4599294d5328a06e12391
John Claro <jkrclaro@gmail.com>
2021-04-25 21:36:52 +0100

    Move labels to bottom

--- helpers/heatmaps.py
+++ helpers/heatmaps.py
@@ -47,7 +47,7 @@ def create_heatmap(data, row_labels, col_labels, ax=None, cbar_kw={},
     ax.set_yticklabels(row_labels)
 
     # Let the horizontal axes labeling appear on top.
-    ax.tick_params(top=True, bottom=False, labeltop=True, labelbottom=False)
+    ax.tick_params(top=False, bottom=True, labeltop=False, labelbottom=True)
 
     # Rotate the tick labels and set their alignment.
     plt.setp(ax.get_xticklabels(), ha='right', rotation_mode='anchor')
