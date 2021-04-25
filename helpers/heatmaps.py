a34deff3f5d903c8be8dada839a67baf1645d727
John Claro <jkrclaro@gmail.com>
2021-04-25 15:27:58 +0100

    Setup helpers for generating heatmaps

--- helpers/heatmaps.py
+++ helpers/heatmaps.py
@@ -1,10 +1,13 @@
 import numpy as np
-import matplotlib
+import pandas as pd
 import matplotlib.pyplot as plt
 
+numbers = ['1', '2', '3', '4', '5', '6', '7', '8']
+alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
 
-def heatmap(data, row_labels, col_labels, ax=None,
-            cbar_kw={}, cbarlabel="", **kwargs):
+
+def create_heatmap(data, row_labels, col_labels, ax=None, cbar_kw={},
+                   cbarlabel="", **kwargs):
     """
     Create a heatmap from a numpy array and two lists of labels.
 
@@ -63,60 +66,16 @@ def heatmap(data, row_labels, col_labels, ax=None,
     return im, cbar
 
 
-def annotate_heatmap(im, data=None, valfmt="{x:.2f}",
-                     textcolors=("black", "white"),
-                     threshold=None, **textkw):
-    """
-    A function to annotate a heatmap.
+def generate_rows(piece):
+    df = pd.read_csv(f'csvs/{piece}.csv')
+    rows = []
+    for number in reversed(numbers):
+        row = []
+        for alphabet in alphabets:
+            key = f'{alphabet}{number}'
+            value = df[df['Location'] == key]['Count'].values[0]
+            row.append(value)
+            if alphabet == 'h':
+                rows.append(row)
 
-    Parameters
-    ----------
-    im
-        The AxesImage to be labeled.
-    data
-        Data used to annotate.  If None, the image's data is used.  Optional.
-    valfmt
-        The format of the annotations inside the heatmap.  This should either
-        use the string format method, e.g. "$ {x:.2f}", or be a
-        `matplotlib.ticker.Formatter`.  Optional.
-    textcolors
-        A pair of colors.  The first is used for values below a threshold,
-        the second for those above.  Optional.
-    threshold
-        Value in data units according to which the colors from textcolors are
-        applied.  If None (the default) uses the middle of the colormap as
-        separation.  Optional.
-    **kwargs
-        All other arguments are forwarded to each call to `text` used to create
-        the text labels.
-    """
-
-    if not isinstance(data, (list, np.ndarray)):
-        data = im.get_array()
-
-    # Normalize the threshold to the images color range.
-    if threshold is not None:
-        threshold = im.norm(threshold)
-    else:
-        threshold = im.norm(data.max())/2.
-
-    # Set default alignment to center, but allow it to be
-    # overwritten by textkw.
-    kw = dict(horizontalalignment="center",
-              verticalalignment="center")
-    kw.update(textkw)
-
-    # Get the formatter in case a string is supplied
-    if isinstance(valfmt, str):
-        valfmt = matplotlib.ticker.StrMethodFormatter(valfmt)
-
-    # Loop over the data and create a `Text` for each "pixel".
-    # Change the text's color depending on the data.
-    texts = []
-    for i in range(data.shape[0]):
-        for j in range(data.shape[1]):
-            kw.update(color=textcolors[int(im.norm(data[i, j]) > threshold)])
-            text = im.axes.text(j, i, valfmt(data[i, j], None), **kw)
-            texts.append(text)
-
-    return texts
+    return rows
